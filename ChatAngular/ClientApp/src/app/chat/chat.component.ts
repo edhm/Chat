d18aa77a4e37e52d266f7ab149d422dbac39ca6b
EHM <ed.huerta.m@gmail.com>
2020-09-09 22:38:49 -0500

    ChatComponent _OK

--- ChatAngular/ClientApp/src/app/chat/chat.component.ts
+++ ChatAngular/ClientApp/src/app/chat/chat.component.ts
@@ -1,1 +1,10 @@
-﻿
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chat-app',
+  templateUrl: "./chat.component.html"
+})
+
+export class ChatComponent  {
+  public nameDev = "EHM-Architec"
+}
