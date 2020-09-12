c9079ddd0157937b14db6cc3ef211c8e94f7564c
EHM <ed.huerta.m@gmail.com>
2020-09-12 12:39:36 -0500

    Agregar archivos de proyecto.

---
+++ ChatAngular/ClientApp/src/app/chat/chat.component.ts
@@ -0,0 +1,24 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { error } from 'protractor';
+
+@Component({
+  selector: 'chat-app',
+  templateUrl: "./chat.component.html"
+})
+
+export class ChatComponent  {
+  public nameDev: string[];
+  constructor(http: HttpClient, @Inject("BASE_URL") baseUrl: string)
+  {
+    http.get<Mesage[]>(baseUrl + "api/Chat/Message").subscribe(result => {
+      this.nameDev = result;
+    }, error => console.error(error));
+  }
+}
+
+interface Message {
+  Id: number,
+  Name: string,
+  Text: string;
+}                                  
---
+++ ChatAngular/ClientApp/src/app/message/message.component.ts
@@ -0,0 +1,15 @@
+import { Component,Input } from '@angular/core';
+//import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+@Component({
+  selector: 'app-message',
+  templateUrl: "./message.component.html"
+})
+
+export class MessageComponent {
+  @Input() oMessage:Message;
+}
+
+interface Message {
+  Id: number;
+  Name: string;
+  Text: string;}
